d35f4590aafdbc01967c1e64db38e8f00eb46a37
Aman Soudagar <96035610+amansoudagar@users.noreply.github.com>
2024-09-03 10:57:07 +0530

    Sections fill the program window

--- frontend/issues.py
+++ frontend/issues.py
@@ -1,4 +1,7 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QPushButton, QTableWidgetItem, QMessageBox
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QHeaderView  # Import QHeaderView for ResizeMode
+
 
 class IssueManagement(QWidget):
     def __init__(self):
@@ -6,12 +9,23 @@ class IssueManagement(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
+        # Issues Table
         self.issues_table = QTableWidget()
         self.issues_table.setRowCount(0)
         self.issues_table.setColumnCount(4)
         self.issues_table.setHorizontalHeaderLabels(['Issue', 'Severity', 'Description', 'Status'])
+
+        # Adjusting column widths to fit the window size
+        self.issues_table.horizontalHeader().setStretchLastSection(True)
+        self.issues_table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)  # Issue column
+        self.issues_table.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)  # Severity column
+        self.issues_table.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)  # Description column
+        self.issues_table.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)  # Status column
+
+        # Add the table to the layout
         self.layout.addWidget(self.issues_table)
 
+        # Mark as Resolved Button
         self.mark_resolved_button = QPushButton("Mark as Resolved")
         self.mark_resolved_button.clicked.connect(self.mark_resolved)
         self.layout.addWidget(self.mark_resolved_button)
@@ -19,5 +33,7 @@ class IssueManagement(QWidget):
     def mark_resolved(self):
         selected_row = self.issues_table.currentRow()
         if selected_row != -1:
-            self.issues_table.setItem(selected_row, 3, QTableWidgetItem("Resolved"))
+            item = QTableWidgetItem("Resolved")
+            item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.issues_table.setItem(selected_row, 3, item)
             QMessageBox.information(self, "Issue Resolved", "Issue has been marked as resolved.")
